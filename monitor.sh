4582ac4ba03bf99a6aa16d658166cfeb9c75c5f3
cuongnguyen91 <nguyencaocuong3333@gmail.com>
2017-07-04 01:15:27 -0700

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -3,6 +3,6 @@
 IFS="," read -r -a dir_mon <<< "$dir_monitor"
 IFS="," read -r -a mail_rp <<< "$mail"
 IFS="," read -r -a fr_rp <<< "$frequency_report"
-
+IFS="," read -r -a fr_bu <<< "$frequency_backup_rp"
 tbegin=$(date +%s)
 
