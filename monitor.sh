2826f449502a9dd71d1e198319c968eeb132b859
cuongnguyen91 <nguyencaocuong3333@gmail.com>
2017-07-04 01:55:05 -0700

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -6,10 +6,13 @@ IFS="," read -r -a mail_rp <<< "$mail"
 IFS="," read -r -a fr_rp <<< "$frequency_report"
 IFS="," read -r -a fr_bu <<< "$frequency_backup_rp"
 #check value
-if ( ! -d $dir_mon[i] )
-then
-  echo "diretory not exist"
-  exit 1
-fi
+for i in (0..${#dir_mon})
+do
+  if ( ! -d $dir_mon[i] )
+  then
+    echo "diretory not exist"
+    exit 1
+  fi
+done
 tbegin=$(date +%s)
 
