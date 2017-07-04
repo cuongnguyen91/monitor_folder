b638a63797ef273bd0a5c1be6640ce452d7016d0
cuongnguyen91 <nguyencaocuong3333@gmail.com>
2017-07-04 01:49:28 -0700

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 . /monitor.config
+#get array from value of variable
 IFS="," read -r -a dir_mon <<< "$dir_monitor"
 IFS="," read -r -a mail_rp <<< "$mail"
 IFS="," read -r -a fr_rp <<< "$frequency_report"
 IFS="," read -r -a fr_bu <<< "$frequency_backup_rp"
+#check value
+if ( ! -d $dir_mon[i] )
+then
+  echo "diretory not exist"
+  exit 1
+fi
 tbegin=$(date +%s)
 
